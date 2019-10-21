35ae7aae97b6ac0a4968b62336c240f2c549f12b
Константин Дьяченко <konard@me.com>
2019-10-21 21:23:35 +0300

    Comments added.

--- Platform.Hardware.Cpu/CacheLine.cs
+++ Platform.Hardware.Cpu/CacheLine.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
-
 namespace Platform.Hardware.Cpu
 {
+    /// <summary>
+    /// <para>Contains constants related to CPUs cache line.</para>
+    /// <para>Содержит константы, относящиеся к строке кэша ЦП.</para>
+    /// </summary>
     public static class CacheLine
     {
+        /// <summary>
+        /// <para>Gets the size of CPUs cache line in bytes.</para>
+        /// <para>Получает размер строки кэша ЦП в байтах.</para>
+        /// </summary>
         public static readonly int Size = GetSize();
 
         private static int GetSize()
